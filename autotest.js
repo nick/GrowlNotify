7a23d2e6d37d77edbddc2018fd76fc4dd615d129
Ed Spencer <edward@domine.co.uk>
2010-11-22 01:56:51 -0800

    Fixed some formatting

--- autotest.js
+++ autotest.js
@@ -1,5 +1,5 @@
 // Change this to the path of your source files
-var srcDir = "/Users/nick/Projects/sencha/Mobile/src/platform/src";
+var srcDir = "/Users/ed/Projects/extjs/touch/src/platform/src";
 var playSounds = true;
 var port = 3001; // Socket.IO listener port
 
@@ -23,29 +23,40 @@ var socket = io.listen(server);
 socket.on('connection', function(client){
   client.on('message', function(msg){ 
       var running = msg.match(/RUNNING/),
-          errors = msg.match(/ERRORS: ([0-9]+)/);
-      if(running) {
-          if(playSounds) exec('afplay run.mp3');
+          errors = msg.match(/ERRORS: ([0-9]+)/).
+          msg, img;
+         
+      if (running) {
+          if (playSounds) {
+              exec('afplay run.mp3');
+          }
       } else if(errors) {
-          if(errors[1] != "0") { 
-              if(playSounds) exec('afplay fail.mp3');
-              var msg = errors[1] + ' failures';
-                  img = "fail.png";
+          if (errors[1] != "0") { 
+              if (playSounds) {
+                  exec('afplay fail.mp3');
+              }
+              
+              msg = errors[1] + ' failures';
+              img = "fail.png";
+                  
           } else {
-              var msg = "All tests pass";
-                  img = "pass.png";
+              msg = "All tests pass";
+              img = "pass.png";
           }
           exec('growlnotify -n SenchaTest --image=' + img + ' -m "' + msg + '" Sencha Autotest');
       }
-  })
+  });
 });
 
 child = exec('find ' + srcDir + ' | grep \.js', 
     function (err, stdout, stderr) {
-        if(err || stderr) { sys.puts(err || stderr); process.exit(); }
+        if (err || stderr) {
+            sys.puts(err || stderr); process.exit();
+        }
+        
         var files = stdout.split("\n");
-        for(var i=0; i< files.length; i++) {
-            (function(path) {                    
+        for (var i=0; i< files.length; i++) {
+            (function(path) {    
                 fs.watchFile(path, {persistent: true, interval: 200}, function (curr, prev) {
                     if(String(curr.mtime) != String(prev.mtime)) {
                         sys.puts("Detected change on " + path + " - updating client");
@@ -53,6 +64,6 @@ child = exec('find ' + srcDir + ' | grep \.js',
                     }
                 });
             })(files[i]);
-        }            
+        }
     }
 );
